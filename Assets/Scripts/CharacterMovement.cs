20a636e6a7bf2f974f10de63dbb146fde5bacd7c
Taylor Moore <taylorgamedev@gmail.com>
2017-01-05 02:02:11 +0430

    Basic player input and movement. Basic camera movement. Two scenes added, one for overworld and one for battles. Development art added for environment, player, enemies, minions, projectiles, doors and portals.

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/** This class reaches out to the Player Input game object and applies basic movement
+  * based off of that. The names and properties of our Input axes and buttons are found in
+  * Edit >> Project Settings >> Input. */
+
+public class CharacterMovement : MonoBehaviour {
+
+	PlayerInput playerInput;
+
+	[SerializeField]
+	float movementSpeed;
+
+	void Start () {
+		playerInput = GameObject.Find("Player Input").GetComponent<PlayerInput>();
+	}
+	
+	void Update () {
+
+		transform.Translate(Vector2.up * playerInput.verticalAxis * movementSpeed * Time.deltaTime);
+
+		transform.Translate(Vector2.right * playerInput.horizontalAxis * movementSpeed * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/** This script is attached to the Player Input game object that is included in every 
+  * scene. My thinking was that any other object that requires player input (e.g. player,
+  * UI stuff, battling stuff) will all be able to reach to this object to get whatever
+  * input that they need, rather than hard coding player input into each game object. 
+  * The names and properties of our Input axes and buttons are found in Edit >> 
+  * Project Settings >> Input. */
+
+public class PlayerInput : MonoBehaviour {
+
+	public float horizontalAxis = 0;
+	public float verticalAxis = 0;
+	
+	void Start() {
+		DontDestroyOnLoad(gameObject);
+	}
+
+	void Update () {
+		horizontalAxis = Input.GetAxis("Horizontal");
+		verticalAxis = Input.GetAxis("Vertical");
+	}
+}
